2bcb4974d39b6b00ffae8422786b0fddb0467e8a
Maryam Mostashar <mostashari2009@gmail.com>
2022-04-30 14:51:34 +0430

    start git in project

---
+++ src/Dashboard.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+
+export default () => (
+    <Card>
+        <CardHeader title="صفحه مدیریت" />
+        <CardContent>نمودارها و عکس ها...</CardContent>
+    </Card>
+);
