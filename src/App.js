d6ee320f82eddbb5d1663cdd395a7cddbefe4f9d
vahidqo <69747871+vahidqo@users.noreply.github.com>
2022-05-06 17:25:27 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { theme } from "./theme";
 import myDataProvider from './dataProvider';
 import drfProvider, { tokenAuthProvider, fetchJsonWithAuthToken, jwtTokenAuthProvider, fetchJsonWithAuthJWTToken } from 'ra-data-django-rest-framework';
 
-let authProvider = jwtTokenAuthProvider({obtainAuthTokenUrl: "http://185.231.115.209:8080//PMWorks/token/"});
+let authProvider = jwtTokenAuthProvider({obtainAuthTokenUrl: "http://185.231.115.209:8080/PMWorks/token/"});
 
 const App = () => (
     <Admin dashboard={Dashboard} theme={theme} disableTelemetry authProvider={authProvider} dataProvider={myDataProvider}>
