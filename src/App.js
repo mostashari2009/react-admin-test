4a44c8920ac8344c1c0f0087287102084323a4fd
mostashari2009 <104681327+mostashari2009@users.noreply.github.com>
2022-05-21 08:28:08 +0430

    Update App.js
    از خط 20 کدهای جدید اضافه شده

--- src/App.js
+++ src/App.js
@@ -18,34 +18,36 @@ export default App;
 ////////////////////////////////////////
 
 import * as React from "react";
-import { Admin, Resource,ListButton } from 'react-admin';
-import { theme } from "./theme";
 import myDataProvider from './dataProvider';
+import {Admin, Resource} from 'react-admin';
 import Dashboard from './Dashboard';
 import { PostList, PostCreate, PostEdit, PostShow } from './posts';
+import { UserList } from './users';
 import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
 import farsiMessages from 'ra-language-farsi';
 import polyglotI18nProvider from 'ra-i18n-polyglot';
+import drfProvider, { tokenAuthProvider, fetchJsonWithAuthToken, jwtTokenAuthProvider, fetchJsonWithAuthJWTToken } from 'ra-data-django-rest-framework';
 
+let authProvider = jwtTokenAuthProvider({obtainAuthTokenUrl: "http://185.231.115.209:8080/PMWorks/token/"});
 
 const messages = {
     'fa': farsiMessages,
-};
-
+}
 const i18nProvider = polyglotI18nProvider(locale => messages[locale], 'fa');
-
-const CommentListButton = () => (
-    <ListButton basePath="/comments" label="Comments" />
-);  
-
+ 
 
 const App = () => (
-    <Admin  dataProvider={myDataProvider}
+    <Admin
+            dataProvider={myDataProvider}
             i18nProvider={i18nProvider}
-            theme={theme}
-            dashboard={Dashboard} >
-      <Resource name="posts" list={PostList} create={PostCreate} edit={PostEdit} show={PostShow} icon={PostIcon} />
+            dashboard={Dashboard} 
+            authProvider={authProvider} >    
+            <Resource name="PMWorks/Delay" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}  show={PostShow} />
+            <Resource name="users" list={UserList} icon={UserIcon} />
+      
     </Admin>
 );
-
 export default App;
+
+
