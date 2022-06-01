007f75baecf3e185ec3fb5b9c7c94a3fbce7909d
mostashari2009 <104681327+mostashari2009@users.noreply.github.com>
2022-06-01 23:42:49 +0430

    Update App.js
    کدهای جدید بعد از خط 20 شروع می شود

--- src/App.js
+++ src/App.js
@@ -21,12 +21,13 @@ import * as React from "react";
 import myDataProvider from './dataProvider';
 import {Admin, Resource} from 'react-admin';
 import Dashboard from './Dashboard';
-import { PostList, PostCreate, PostEdit, PostShow } from './posts';
+import { DelayList, DelayCreate, DelayEdit, DelayShow} from './delay';
 import { UserList } from './users';
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 import farsiMessages from 'ra-language-farsi';
 import polyglotI18nProvider from 'ra-i18n-polyglot';
+import { Theme } from "./theme";
 import drfProvider, { tokenAuthProvider, fetchJsonWithAuthToken, jwtTokenAuthProvider, fetchJsonWithAuthJWTToken } from 'ra-data-django-rest-framework';
 
 let authProvider = jwtTokenAuthProvider({obtainAuthTokenUrl: "http://185.231.115.209:8080/PMWorks/token/"});
@@ -42,12 +43,15 @@ const App = () => (
             dataProvider={myDataProvider}
             i18nProvider={i18nProvider}
             dashboard={Dashboard} 
+            theme={Theme}
             authProvider={authProvider} >    
-            <Resource name="PMWorks/Delay" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}  show={PostShow} />
-            <Resource name="users" list={UserList} icon={UserIcon} />
+            <Resource name="PMWorks/Delay" options={{ label: 'تاخیر' }} list={DelayList} edit={DelayEdit} create={DelayCreate} icon={PostIcon}  show={DelayShow}  
+            />
+            <Resource name="users" options={{ label: 'تجهیزات' }}  list={UserList} icon={UserIcon} />
       
     </Admin>
 );
 export default App;
+
 
 
