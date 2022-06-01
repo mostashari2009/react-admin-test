aae9765c63becbb368a77ac453ca4214364a5d57
mostashari2009 <104681327+mostashari2009@users.noreply.github.com>
2022-06-02 00:04:05 +0430

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+import './fonts/B Nazanin.ttf' ;
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
