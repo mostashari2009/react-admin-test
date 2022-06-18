b699b4dd2aeba0f92ec9ec8284431833bc2b1feb
mostashari2009 <104681327+mostashari2009@users.noreply.github.com>
2022-06-18 13:58:04 +0430

    Create theme.js

---
+++ src/theme.js
@@ -0,0 +1,9 @@
+import { createTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
+
+  export const Theme = createTheme({
+  typography: { 
+  fontFamily: 'B Nazanin, Arial',
+  },
+});
