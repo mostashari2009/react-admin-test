883f8922cbb3644fd53dd0f0d9df1093c7f8a502
mostashari2009 <104681327+mostashari2009@users.noreply.github.com>
2022-06-18 13:51:43 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
 import * as React from "react";
-import { Admin, Resource  } from 'react-admin';
-import Dashboard from "./Dashboard";
-import { theme } from "./theme";
 import myDataProvider from './dataProvider';
-import drfProvider, { tokenAuthProvider, fetchJsonWithAuthToken, jwtTokenAuthProvider, fetchJsonWithAuthJWTToken } from 'ra-data-django-rest-framework';
-
-let authProvider = jwtTokenAuthProvider({obtainAuthTokenUrl: "http://185.231.115.209:8080/PMWorks/token/"});
-
-const App = () => (
-    <Admin dashboard={Dashboard} theme={theme} disableTelemetry authProvider={authProvider} dataProvider={myDataProvider}>
-         <Resource />
-    </Admin>
-);
-
-export default App;
-
-////////////////////////////////////////
-
-import * as React from "react";
-import myDataProvider from './dataProvider';
-import {Admin, Resource} from 'react-admin';
+import {Admin, Resource, Layout } from 'react-admin';
 import Dashboard from './Dashboard';
 import { DelayList, DelayCreate, DelayEdit, DelayShow} from './delay';
-import { UserList } from './users';
+import {UserList } from './users';
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 import farsiMessages from 'ra-language-farsi';
 import polyglotI18nProvider from 'ra-i18n-polyglot';
-import { Theme } from "./theme";
+import {Theme } from "./theme";
 import drfProvider, { tokenAuthProvider, fetchJsonWithAuthToken, jwtTokenAuthProvider, fetchJsonWithAuthJWTToken } from 'ra-data-django-rest-framework';
+import AddBoxOutlinedIcon from '@material-ui/icons/AddBoxOutlined';
+import TreeMenu from '@bb-tech/ra-treemenu';
+import MyLoginPage from './MyLoginPage';
 
 let authProvider = jwtTokenAuthProvider({obtainAuthTokenUrl: "http://185.231.115.209:8080/PMWorks/token/"});
 
@@ -39,19 +23,20 @@ const i18nProvider = polyglotI18nProvider(locale => messages[locale], 'fa');
  
 
 const App = () => (
-    <Admin
+    <Admin  disableTelemetry theme={Theme} layout={(props) => <Layout {...props} menu={TreeMenu}  />}
+            loginPage={MyLoginPage}
             dataProvider={myDataProvider}
             i18nProvider={i18nProvider}
             dashboard={Dashboard} 
-            theme={Theme}
-            authProvider={authProvider} >    
-            <Resource name="PMWorks/Delay" options={{ label: 'تاخیر' }} list={DelayList} edit={DelayEdit} create={DelayCreate} icon={PostIcon}  show={DelayShow}  
-            />
+            authProvider={authProvider} 
+            menu={TreeMenu}
+            >
+            <Resource name="modiriat" icon={AddBoxOutlinedIcon} options={{ "label": "پنل کاربری", "isMenuParent": true }} />        
+            <Resource name="PMWorks/Delay" options={{ label: 'تاخیرات' }} list={DelayList} edit={DelayEdit} create={DelayCreate} icon={PostIcon}  show={DelayShow} />
             <Resource name="users" options={{ label: 'تجهیزات' }}  list={UserList} icon={UserIcon} />
       
     </Admin>
 );
 export default App;
-
 
 
